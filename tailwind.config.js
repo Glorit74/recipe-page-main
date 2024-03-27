89f2121c6cd1587f2286d51f8656c0a82cf592ab
Flora <laszlo.t.flora@gmail.com>
2024-03-27 18:15:37 +0100

    colors, fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: "375px",
+      md: "750px",
+      lg: "1440px",
+    },
+    colors: {
+      primary: "hsl(14, 45%, 36%)",
+      primaryDark: "hsl(332, 51%, 32%)",
+      white: "hsl(0, 0%, 100%)",
+      roseWhite: "hsl(330, 100%, 98%)",
+      eggshell: "hsl(30, 54%, 90%)",
+      grey: "hsl(30, 18%, 87%)",
+      brown: "hsl(30, 10%, 34%)",
+      charcoalDark: "hsl(24, 5%, 18%)",
+    },
+
+    fontFamily: {
+      serif: ["Young", "serif"],
+      sans: ["Outfit", "sans-serif"],
+    },
+    fontSize: {
+      normal: "16px",
+    },
     extend: {},
   },
   plugins: [],
 };
-
